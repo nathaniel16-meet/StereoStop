05b329bbf607be5e7df8f0f743cf68fc4a8f92d1
Nathaniel Navarro <nathaniel16@meet.mit.edu>
2016-07-21 15:03:09 +0300

    created static folder

--- StereoStop.py
+++ StereoStop.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,13 +7,15 @@ Marvin = {
   'name': "Nate"
 }
 
+pics = [ 'main-qimg-81ed258627de52b9eaaec07423435aff-c.jpg', 'dina.jpg', 'ballet.jpg', '160310_arabs.jpg']
+
 @app.route ('/')
 def home():
 	return render_template('base.html')
 
 @app.route('/profile')
 def profile():
-  return render_template('profile.html', user = Marvin)
+  return render_template('profile.html', user = Marvin, pics = pics)
 
 
 @app.errorhandler(404)
